b266e53c5c1b6d046abffd4c2bf777a21343e867
mal-wood <mallorywood413@gmail.com>
2022-03-02 19:31:15 -0800

    Updates

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-resume-js": () => import("./../../../src/pages/resume.js" /* webpackChunkName: "component---src-pages-resume-js" */),
-  "component---src-pages-work-js": () => import("./../../../src/pages/work.js" /* webpackChunkName: "component---src-pages-work-js" */)
-}
-
--- README.md
+++
@@ -1,1 +0,0 @@
-My personal website. Built with Gatsby and Ant Design.
